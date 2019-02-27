a3e73c91b307e72be5b7a50624079224c56ad555
Diego Souza <diegosouza@tradecom.com.br>
2019-02-27 10:56:58 -0300

    Modal para Logout

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionService } from '../../resources/services/session.service';
+import { ModalService } from '../modal/modal.service';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 
 @Component({
@@ -10,15 +12,28 @@ import { SessionService } from '../../resources/services/session.service';
 })
 export class MenuComponent implements OnInit {
 
-  constructor(private router: Router, private sessionService: SessionService) { }
+  bsModalRef: BsModalRef
+  constructor(private router: Router, private sessionService: SessionService, modalService: ModalService, private bsModalService: BsModalService) { }
 
   ngOnInit() {
   }
 
   logout() {
-    this.router.navigateByUrl['login'];
-    this.router.navigate(['login'])
     this.sessionService.clear();
+    this.router.navigate(['login'])
     console.log('SAINDO')
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.bsModalRef = this.bsModalService.show(template, { class: 'modal-sm' });
+  }
+
+  confirm(): void {
+    this.logout();
+    this.bsModalRef.hide();
+  }
+
+  decline(): void {
+    this.bsModalRef.hide();
   }
 }

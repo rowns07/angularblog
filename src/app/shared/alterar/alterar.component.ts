30205133a0cab370802c0540e001932d48317655
Diego Souza <diegosouza@tradecom.com.br>
2019-03-02 03:53:51 -0300

    Adicionando metodo de fechar o modal

--- src/app/shared/alterar/alterar.component.ts
+++ src/app/shared/alterar/alterar.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { WordpressBi } from '../../resources/bi/wordpress.bi';
-import { WordpressService } from '../../resources/services/wordpress.service';
 import { HttpService } from '../../resources/services/http.service';
+import { WordpressService } from '../../resources/services/wordpress.service';
+import { AlertService } from '../alert/alert.service';
+import { ModalService } from '../modal/modal.service';
 
 @Component({
   selector: 'app-alterar',
@@ -10,19 +12,26 @@ import { HttpService } from '../../resources/services/http.service';
 })
 export class AlterarComponent implements OnInit {
 
-  constructor(private wpService:WordpressService, private httpService:HttpService) { }
+  constructor(
+    private wpService: WordpressService,
+    private httpService: HttpService,
+    private alertService: AlertService,
+    private modalService: ModalService
+  ) { }
 
   ngOnInit() {
   }
 
   public alterar() {
     new WordpressBi.RestWordpress(this.httpService).updatePost(this.wpService.getCurrentResponsePost())
-    .subscribe(
-      success =>{
-        console.log('deu certo o testinho')
-      },error =>{
-        console.log('deu merda no testinho')
-
-      })
+      .subscribe(
+        success => {
+          console.log('deu certo o testinho');
+          this.alertService.showAlertSuccess('Post alterado com sucesso.');
+          this.modalService.close();
+        }, error => {
+          console.log('deu merda no testinho');
+          this.alertService.showAlertSuccess('Problemas ao alterar post.');
+        })
   }
 }
